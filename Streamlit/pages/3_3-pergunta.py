4719de35d3c8902ce97caafbf639d72740a49a15
lucaslima433 <lucaslima433@hotmail.com>
2024-01-30 00:17:52 -0300

    fix: pergunta 3

--- Streamlit/pages/3_3-pergunta.py
+++ Streamlit/pages/3_3-pergunta.py
@@ -5,8 +5,9 @@ import plotly.express as px
 import plotly.graph_objects as go
 import os
 
+
 st.set_page_config(
-  page_title="Pergunta2"
+  page_title="Pergunta 3"
 )
 
 
@@ -21,26 +22,34 @@ def main():
 
   municipio_selecionado = st.selectbox ("Selecione um município:", municipios.municipio)
 
-  st.write("Você selecionou:", municipio_selecionado)
+  #st.write("Você selecionou:", municipio_selecionado)
 
-  # Criando um dicionário de mapeamento de municípios para IDs
   mapa_municipios_ids = dict(zip(municipios['municipio'], municipios['id']))
 
-  # Obtendo o ID correspondente
   id_municipio = mapa_municipios_ids[municipio_selecionado]
-  st.write(f"ID do município {municipio_selecionado}: {id_municipio}")
+  #st.write(f"ID do município {municipio_selecionado}: {id_municipio}")
 
   linhas_filtradas = fatos[fatos['dim_localizacao_id'] == id_municipio]
-  st.write(linhas_filtradas)
 
   nome_dos_ngc = orgaos[['nome_orgao', 'id']]
   dados_com_nomes_orgaos = pd.merge(nome_dos_ngc, linhas_filtradas, left_on='id', right_on='dim_orgao_id')
 
   contagem_por_orgao = dados_com_nomes_orgaos['nome_orgao'].value_counts()
   
-
   st.write("Contagem de licitações por orgao:")
-  st.write(contagem_por_orgao)
-  
+
+  y_orgao = nome_dos_ngc['nome_orgao'].iloc[1:]
+  fig = go.Figure(go.Bar(
+              x= contagem_por_orgao,
+              y= y_orgao,
+              orientation='h'))
+  st.plotly_chart(fig)
+
+  #st.write(contagem_por_orgao)
+
+  st.write("Todas as licitações realizadas no municipio:")
+  st.write(dados_com_nomes_orgaos)
+
+
 if __name__ == '__main__':
   main()
